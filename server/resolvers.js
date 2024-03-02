086aa00193628f9fc6bb56dd973bc731316af93b
Rafael Mendes <rafael.mendes@x-team.com>
2024-03-02 18:54:34 +0000

    server: add `jobs` query and resolver with an extra field `date`

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,19 @@
+import { getCompany } from "./db/companies.js";
+import { getJobs } from "./db/jobs.js";
+
 const resolvers = {
   Query: {
-    greeting: () => "Hello, world!",
+    jobs: () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => toISO8601Date(job.createdAt),
+    company: (job) => getCompany(job.companyId),
   },
 };
+
+function toISO8601Date(dateString) {
+  return dateString.slice(0, "yyyy-mm-dd".length);
+}
 
 export default resolvers;
